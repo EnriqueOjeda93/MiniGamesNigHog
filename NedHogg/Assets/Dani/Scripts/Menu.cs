e212378fafa2064b330b373607d043ff633c44a6
Dgpes <oportunitatzero@gmail.com>
2020-02-25 17:26:58 +0100

    Main menu and pause menu.

---
+++ NedHogg/Assets/Dani/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play(){
+        SceneManager.LoadScene("Map_01");
+    }
+
+    public void Credits(){
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
