b0447c170433279d1c57d42aada88e37c3917e39
Dgpes <oportunitatzero@gmail.com>
2020-02-25 18:29:35 +0100

    Win condittion added.

---
+++ NedHogg/Assets/Dani/Scripts/WinScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour
+{
+   public void Win1() {
+        SceneManager.LoadScene("Player1Win");
+   }
+   public void Win2() {
+        SceneManager.LoadScene("Player2Win");   
+   }
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       if(other.tag == "Player"){
+           Win1();
+       }
+       /*if(other.tag == "Player2"){
+           Win2();
+       }*/
+   }
+
+   public void PlayAgain() {
+        SceneManager.LoadScene("Map_01");
+   }
+
+   public void Menu() {
+        SceneManager.LoadScene("Menu");
+   }
+
+}
